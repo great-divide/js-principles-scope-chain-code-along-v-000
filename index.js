f250418345da2d061aecd755877c8a533b7b63e3
Brian Birdwell <bbirdwell@gmail.com>
2018-12-18 21:42:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function setBestCustomer() {
 function overWriteBestCustomer() {
   bestCustomer = 'maybe bob'
 }
+
+const leastFavoriteCustomer = 'bill'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy bob'
+}
