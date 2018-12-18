fd11776952368689b1e11ef5fa4c5917d32d6422
Brian Birdwell <bbirdwell@gmail.com>
2018-12-18 21:39:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ var customerName = 'bob'
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
